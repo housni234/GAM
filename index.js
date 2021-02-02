e5284168e2648f5cd623918ce2dc1656c1819252
Housni <alami.housni@gmail.com>
2021-02-02 15:24:01 +0100

    stpm form submiting in progress

---
+++ index.js
@@ -0,0 +1,31 @@
+
+//const contactForm = document.getElementById("contact-us-form");
+
+document.getElementById("contact-us-form").addEventListener("submit", submitForm);
+
+
+/**/function submitForm() {
+
+    //get the email values
+    let name = document.getElementById("InputName").value;
+    let subject = document.getElementById("InputSubject").value;
+    let email = document.getElementById("InputEmail").value;
+    let message = document.getElementById("message-text").value;
+
+    document.getElementById("contact-us-form").reset();
+    sendEmail(name, subject, email, message)
+}
+
+function sendEmail(name, subject, email, message) {
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: "testing84.email@gmail.com",
+        Password: "qwertyu-99",
+        To: 'testing84.email@gmail.com',
+        From: "testing84.email@gmail.com",
+        Subject: subject,
+        Body: email, name, message,
+    }).then(
+        message => alert("Your email has been sent successfully"));
+}
+
