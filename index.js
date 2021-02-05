43c8f78d50986db617e4a689c9cd23d3896949e7
Housni <alami.housni@gmail.com>
2021-02-05 12:38:33 +0100

    smtp almost done

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ document.getElementById("contact-us-form").addEventListener("submit", submitForm
 
 
 /**/function submitForm() {
-
     //get the email values
     let name = document.getElementById("InputName").value;
     let subject = document.getElementById("InputSubject").value;
@@ -13,7 +12,7 @@ document.getElementById("contact-us-form").addEventListener("submit", submitForm
     let message = document.getElementById("message-text").value;
 
     document.getElementById("contact-us-form").reset();
-    sendEmail(name, subject, email, message)
+    sendEmail(name, subject, email, message);
 }
 
 function sendEmail(name, subject, email, message) {
@@ -24,8 +23,8 @@ function sendEmail(name, subject, email, message) {
         To: 'testing84.email@gmail.com',
         From: "testing84.email@gmail.com",
         Subject: subject,
-        Body: email, name, message,
+        Body: `Name: ${name} <br> Email: ${email} <br> Message: ${message} `
     }).then(
-        message => alert("Your email has been sent successfully"));
+        (message) => alert("Your email has been sent successfully"));
 }
 
