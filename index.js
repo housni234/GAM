5c1ce635e82d1501088952d4316c16ce59a8b93c
Housni <alami.housni@gmail.com>
2021-06-01 11:57:42 +0200

    banner to register page added + hide contact us

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-// toggle button
+// // toggle button
 
-// contact us sending email SMTP
-document.getElementById("contact-us-form").addEventListener("submit", submitForm);
+// // contact us sending email SMTP
+// document.getElementById("contact-us-form").addEventListener("submit", submitForm);
 
 
-function submitForm() {
-    //get the email values
-    let name = document.getElementById("InputName").value;
-    let subject = document.getElementById("InputSubject").value;
-    let email = document.getElementById("InputEmail").value;
-    let message = document.getElementById("message-text").value;
+// function submitForm() {
+//     //get the email values
+//     let name = document.getElementById("InputName").value;
+//     let subject = document.getElementById("InputSubject").value;
+//     let email = document.getElementById("InputEmail").value;
+//     let message = document.getElementById("message-text").value;
 
-    document.getElementById("contact-us-form").reset();
-    sendEmail(name, subject, email, message);
-}
+//     document.getElementById("contact-us-form").reset();
+//     sendEmail(name, subject, email, message);
+// }
 
-function sendEmail(name, subject, email, message) {
-    Email.send({
-        Host: "smtp.gmail.com",
-        Username: "globalacademicmentorscontact@gmail.com",
-        Password: "wnersdpplyfjtilt",
-        To: "globalacademicmentorscontact@gmail.com",
-        From: "globalacademicmentorscontact@gmail.com",
-        Subject: subject,
-        Body: `Name: ${name} <br> Email: ${email} <br> Message: ${message}`,
-    }).then(
-        (message) => alert("Your email has been sent successfully"));
-}
+// function sendEmail(name, subject, email, message) {
+//     Email.send({
+//         Host: "smtp.gmail.com",
+//         Username: "globalacademicmentorscontact@gmail.com",
+//         Password: "wnersdpplyfjtilt",
+//         To: "globalacademicmentorscontact@gmail.com",
+//         From: "globalacademicmentorscontact@gmail.com",
+//         Subject: subject,
+//         Body: `Name: ${name} <br> Email: ${email} <br> Message: ${message}`,
+//     }).then(
+//         (message) => alert("Your email has been sent successfully"));
+// }
 
